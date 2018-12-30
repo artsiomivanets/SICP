50ec0f4201128d3ed27a828f44dd410b4d709e67
artem.ivanec <artem.ivanec96@gmail.com>
2018-12-31 00:11:36 +0300

    add ex

---
+++ sicp/chapter2/2.66/2.66.rkt
@@ -0,0 +1,10 @@
+#lang racket/base
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) #f)
+        ((= given-key (key (entry set-of-records)))
+         (entry set-of-records))
+        ((< given-key (key (entry set-of-records)))
+         (lookup given-key (left-branch set-of-records)))
+        ((> given-key (key (entry set-of-records)))
+         (lookup given-key (right-branch set-of-records)))))
